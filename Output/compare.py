1607216be4b67b5fbac4ff9fd79b550873f9958b
arsenezhang <arsenezhang@126.com>
2018-01-28 17:48:49 +0800

    fix

--- Output/compare.py
+++ Output/compare.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 
 numWorkers = [1,10,20,30,40]
-# files = ["epinions-sh/w10.txt", "fully_asy/ep-sh-10.txt"]
-files = ["epinions-sig/w10.txt", "fully_asy/ep-sig-10.txt"]
+files = ["epinions-sh/w10.txt", "fully_asy/ep-sh-10.txt"]
+# files = ["epinions-sig/w10.txt", "fully_asy/ep-sig-10.txt"]
 # files = ["epinions-sq/w10.txt", "fully_asy/ep-sq-10.txt"]
 labels = ["asy_syn", "asy"]
 speedup = []
@@ -29,12 +29,13 @@ for i in range(2):
 	totTime.append(time[-1])
 	speedup.append(totTime[0] / totTime[-1])
 
+	l = len(time)
 	#plt.plot(time, acc, label = "w = " + str(numWorkers[i]))
-	plt.plot(time, acc, label = labels[i])
+	plt.plot(time[1:int(l)], acc[1:int(l)], label = labels[i])
 	plt.xlabel("Time")
 	#plt.xlabel("Epoch")
 	plt.ylabel("Accuracy")
-	plt.title("Epinions with Square Loss")
+	plt.title("Epinions with Square-Hinge Loss on 10 workers")
 	plt.legend(loc = 0)
 	plt.grid(True)
 
