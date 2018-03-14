dd6a6ca50c37efe07261d39ca7febd1de557d4ae
arsenezhang <arsenezhang@126.com>
2018-03-14 20:45:41 +0800

    fix

--- Output/plot_speedup.py
+++ Output/plot_speedup.py
@@ -1,42 +1,48 @@
 import matplotlib.pyplot as plt
 
-algos = ["Slashdot-Sigmoid", "Slashdot-Square", "Slashdot-Sauare-Hinge", "Epinions-Sigmoid", "Epinions-Square", "Epinions-Square-Hinge"]
-
+algos = ["Slashdot-Sigmoid", "Slashdot-Square", "Slashdot-Sauared Hinge", "Epinions-Sigmoid", "Epinions-Square", "Epinions-Squared Hinge"]
+marker = ['o', '*', 'v', 's','h','+']
 numWorkers = [1,10,20,30,40]
-s = [[], [], [], [], [], []]
+time = [[0 for j in range(5)] for i in range(6)]
+asy_time = [[0 for j in range(5)] for i in range(6)]
+speedup = [[0 for j in range(5)] for i in range(6)]
 
-s[0] = [1.0, 4.296753749033111, 6.281899515727457, 7.390569139188242, 8.47231139745586]
-t0 = [68.8804, 16.0308, 10.9649, 9.32004, 8.13006]
+# Slashdot-Sigmoid
+time[0] = [72.66, 15.43, 11.14, 8.10, 7.42] # 
+asy_time[0] = [70.97, 12.40, 8.72, 7.43, 6.85] #
 
-s[1] = [1.0, 4.124579473783148, 6.025083089509416, 7.071526211089007, 7.523330728221402]
-t1 = [63.6303, 15.4271, 10.5609, 8.9981, 8.45773]
+# Slashdot-Square
+time[1] = [68.44, 14.35, 10.56, 7.83, 7.47] #
+asy_time[1] = [68.38, 12.33, 8.48, 7.23, 6.99] #
 
-s[2] = [1.0, 3.7494084940257895, 5.827195109338911, 6.364917768174407, 6.427204400562745]
-t2 = [50.71, 13.5248, 8.7023, 7.96711, 7.8899] 
+# Slashdot-Sauare-Hinge
+time[2] = [63.80, 13.71, 10.40, 7.60, 6.69] #
+asy_time[2] = [64.59, 11.18, 7.63, 6.77, 6.56] #
 
-s[3] = [1.0, 5.187944805637246, 7.084951704016762, 8.222830706631552, 8.981043916322323]
-t3 = [118.019, 22.7487, 16.6577, 14.3526, 13.1409]
+# Epinions-Sigmoid
+time[3] = [118.61, 23.30, 17.20, 15.26, 12.20] #
+asy_time[3] = [112.40, 19.98, 14.54, 12.21, 11.65]  #
 
-s[4] = [1.0, 4.976024411508282, 7.009383455183928, 7.9882200288667615, 8.587616666797276]
-t4 = [109.584, 22.0224, 15.6339, 13.7182, 12.7607]
+# Epinions-Square
+time[4] = [108.68, 22.41, 16.60, 14.47, 11.69] #
+asy_time[4] = [108.65, 18.79, 13.92, 11.97, 11.29] #
 
-s[5] = [1.0, 4.878158844765343, 6.305745753531201, 7.147051184170136, 8.494134815064264]
-t5 = [90.804, 18.6144, 14.4002, 12.7051, 10.6902]
+# Epinions-Square-Hinge
+time[5] = [104.60, 20.44, 15.13, 12.74, 11.27]  #
+asy_time[5] = [103.60, 18.05, 12.99, 11.18, 10.97] #
+
+for i in range(6):
+	for j in range(5):
+		speedup[i][j] = asy_time[i][0] / asy_time[i][j] 
 
 plt.figure(1)
 for i in range(6):
-	plt.plot(numWorkers, s[i], label = algos[i], marker = 'o')
-	plt.xlabel("Number of workers")
-	plt.ylabel("Speedup")
+	plt.plot(numWorkers, speedup[i], label = algos[i], marker = marker[i])
+	plt.xlabel("Number of workers", fontsize = 14)
+	plt.ylabel("Speedup", fontsize = 14)
+	plt.xticks(fontsize = 14)
+	plt.yticks(fontsize = 14)
 	plt.grid(True)
-	plt.legend(loc = 0)
+	plt.legend(loc = 0, fontsize = 12)
 
 plt.show()
-
-
-
-
-
-
-
-
