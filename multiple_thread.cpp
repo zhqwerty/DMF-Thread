1161c25ef20b61f814cc5d2b825d2349da35b91d
zhqwerty <zhanghan213@163.com>
2018-03-17 13:50:41 +0800

    fiux

--- multiple_thread.cpp
+++ multiple_thread.cpp
@@ -87,19 +87,19 @@ void* gradient_thread(void* params) {
         double predict = FVector::dot(X[row_index], Y[col_index]);
 
         // Apply Gradient for Sigmoid Loss
-//        double den = pow(1 + exp(predict * rating), 2); 
-//        
-//        FVector gradXi = Y[col_index]; // need to multiply Yj
-//        gradXi.scale(-exp(rating * predict) * rating / den);
-//        gradXi.scale_and_add(X[row_index], lambda); 
-//
-//        X[row_index].scale_and_add(gradXi, -cur_learning_rate);
-//
-//        FVector gradYj = X[row_index]; // need to multiply by Xi
-//        gradYj.scale(-exp(rating * predict) * rating / den);
-//        gradYj.scale_and_add(Y[col_index], lambda);
-//
-//        Y[col_index].scale_and_add(gradYj, -cur_learning_rate);
+        double den = pow(1 + exp(predict * rating), 2); 
+        
+        FVector gradXi = Y[col_index]; // need to multiply Yj
+        gradXi.scale(-exp(rating * predict) * rating / den);
+        gradXi.scale_and_add(X[row_index], lambda); 
+
+        X[row_index].scale_and_add(gradXi, -cur_learning_rate);
+
+        FVector gradYj = X[row_index]; // need to multiply by Xi
+        gradYj.scale(-exp(rating * predict) * rating / den);
+        gradYj.scale_and_add(Y[col_index], lambda);
+
+        Y[col_index].scale_and_add(gradYj, -cur_learning_rate);
 
         // Apply Gradient for Square Loss
 //        FVector gradXi = Y[col_index];
@@ -115,21 +115,21 @@ void* gradient_thread(void* params) {
 //        Y[col_index].scale_and_add(gradYj, -cur_learning_rate);
 
         // Apply Gradient for Square-hinge Loss
-        if (rating * predict < 1){
-            FVector gradXi = Y[col_index];
-            gradXi.scale(2 * (rating * predict - 1) * rating);
-            gradXi.scale_and_add(X[row_index], lambda);
-            X[row_index].scale_and_add(gradXi, -cur_learning_rate);
-            
-            FVector gradYj = X[row_index];
-            gradYj.scale(2 * (rating * predict - 1) * rating);
-            gradYj.scale_and_add(Y[col_index], lambda);
-            Y[col_index].scale_and_add(gradYj, -cur_learning_rate);
-        } 
-        else{
-            X[row_index].scale(1 - cur_learning_rate * lambda);
-            Y[col_index].scale(1 - cur_learning_rate * lambda);
-        }
+//        if (rating * predict < 1){
+//            FVector gradXi = Y[col_index];
+//            gradXi.scale(2 * (rating * predict - 1) * rating);
+//            gradXi.scale_and_add(X[row_index], lambda);
+//            X[row_index].scale_and_add(gradXi, -cur_learning_rate);
+//            
+//            FVector gradYj = X[row_index];
+//            gradYj.scale(2 * (rating * predict - 1) * rating);
+//            gradYj.scale_and_add(Y[col_index], lambda);
+//            Y[col_index].scale_and_add(gradYj, -cur_learning_rate);
+//        } 
+//        else{
+//            X[row_index].scale(1 - cur_learning_rate * lambda);
+//            Y[col_index].scale(1 - cur_learning_rate * lambda);
+//        }
         
         sampleCount++; 
     }
@@ -157,11 +157,11 @@ int main(int argv, char *argc[]){
 
     // Variables Update
     int maxEpoch = 100;
-    double learning_rate = 0.02;
+    double learning_rate = 1;
     double cur_learning_rate = learning_rate;
-    int nWorkers = 40;
+    int nWorkers = 1;
     double sample_rate = 0.9;
-    double lambda = 0.4;
+    double lambda = 0.01;
     double maxAcc = 0;
     int nTrain = int(nExamples * sample_rate);
     int nTest = nExamples - nTrain;
